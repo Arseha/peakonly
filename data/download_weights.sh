c461cedab9646a42be5c1b7fec2357fb3c76f9e7
arseha <melnikov.arsenty@gmail.com>
2019-09-10 11:43:41 +0700

    initial commit

--- data/download_weights.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-wget -O Classifier https://getfile.dokpub.com/yandex/get/https://yadi.sk/d/_U_LQBzJnaCcyQ
-wget -O Integrator https://getfile.dokpub.com/yandex/get/https://yadi.sk/d/2sd4l7UJs33RUg
-
